bd67347e7ae7a7c4912403efc34ce73413b8087d
Davi Alpiano <davialpiano@gmail.com>
2026-03-30 18:15:08 -0300

    básico

---
+++ eslint.config.js
@@ -0,0 +1,37 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import prettier from 'eslint-plugin-prettier/recommended'
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended, prettier],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'prettier/prettier': ['error', {
+        'printWidth': 80,
+        'tabWidth': 2,
+        'singleQuote': true,
+        'trailingComma': 'all',
+        'arrowParens': 'always',
+        'semi': true
+      }],
+    },
+  },
+)
