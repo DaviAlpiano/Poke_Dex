ecaaada3aee947d8db08076c08b220de58fc3626
Davi Alpiano <davialpiano@gmail.com>
2026-03-31 18:57:45 -0300

    home inicio

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,8 +5,8 @@ import type { Pokemon } from '../types/api';
 export default function Home() {
   const { pokemonList } = usePoke();
   return (
-    <main className="p-6">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+    <main className="bg-[url('/fundo.png')] bg-cover bg-center bg-no-repeat bg-fixed min-h-screen p-8">
+      <div className="max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {pokemonList.map((pokemon: Pokemon) => (
           <PokeCard name={pokemon.name} url={pokemon.url} key={pokemon.name} />
         ))}
